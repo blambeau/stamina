84571e9420b41be09037c2a9e61bb8e357f455a4
LAMBEAU Bernard <blambeau@cassidy.(none)>
2011-04-28 18:38:46 +0200

    Added --size-tolerance to 'stamina abbadingo-dfa'

--- lib/stamina/quickl_commands/abbadingo_dfa.rb
+++ lib/stamina/quickl_commands/abbadingo_dfa.rb
@@ -12,23 +12,39 @@ module Stamina
       #
       class AbbadingoDfa < Quickl::Command(__FILE__, __LINE__)
         
+        # Size of the target automaton
         attr_accessor :size
+
+        # Tolerance on the size
+        attr_accessor :size_tolerance
 
         # Install options
         options do |opt|
         
-          # Show the help and exit
           @size = 64
           opt.on("--size=X", "Sets the size of the automaton to generate") do |x|
-            @size = x
+            @size = x.to_i
+          end
+
+          @size_tolerance = nil
+          opt.on("--size-tolerance[=X]", Float, "Sets the tolerance on automaton size, in percentage") do |x|
+            @size_tolerance = x.to_f
           end
 
         end # options
 
+        def accept?(dfa)
+          size_tolerance.nil? || (size - dfa.state_count).abs <= (size.to_f*size_tolerance)
+        end
+
         # Command execution
         def execute(args)
           require 'stamina/abbadingo'
-          puts Stamina::Abbadingo::RandomDFA.new(size).execute.to_adl
+          randomizer = Stamina::Abbadingo::RandomDFA.new(size)
+          begin
+            dfa = randomizer.execute
+          end until accept?(dfa)
+          puts dfa.to_adl
         end
         
       end # class AbbadingoDFA
