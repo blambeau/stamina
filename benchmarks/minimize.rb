7a8426fe401e35745a7f7ef5642839dfeebd3267
Bernard Lambeau <blambeau@gmail.com>
2011-09-26 12:01:43 +0200

    Use Stamina::VERSION as variation point

--- benchmarks/minimize.rb
+++ benchmarks/minimize.rb
@@ -1,6 +1,7 @@
 require 'stamina'
 require 'stamina/abbadingo'
 Viiite.bench do |b|
+  b.variation_point :version, Stamina::VERSION
   b.variation_point :ruby, Viiite.which_ruby
   b.range_over([16, 32, 64, 128, 256, 512], :req_size) do |size|
     bench_case = Stamina::Abbadingo::RandomDFA.execute(size, 0.5, :minimize => nil)
