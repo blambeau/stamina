412a0d61a01c1aaa58206518e7008f0d82e2cae4
Bernard Lambeau <blambeau@gmail.com>
2012-02-24 16:50:13 +0100

    Code cleaning.

--- lib/stamina-core/stamina/automaton.rb
+++ lib/stamina-core/stamina/automaton.rb
@@ -160,39 +160,37 @@ module Stamina
       ### public read-only section ###############################################
       public
 
-      #
       # Returns true if this state is an initial state, false otherwise.
-      #
-      def initial?() return false unless @data[:initial]; @data[:initial] end
+      def initial?
+        !!@data[:initial]
+      end
 
-      #
       # Sets this state as an initial state.
-      #
-      def initial!() @data[:initial] = true end
+      def initial!
+        @data[:initial] = true
+      end
 
-      #
       # Returns true if this state is an accepting state, false otherwise.
-      #
-      def accepting?() return false unless @data[:accepting]; @data[:accepting] end
+      def accepting?
+        !!@data[:accepting]
+      end
 
-      #
       # Sets this state as an accepting state.
-      #
-      def accepting!() @data[:accepting] = true end
+      def accepting!
+        @data[:accepting] = true
+      end
 
-      #
       # Returns true if this state is an error state, false otherwise.
-      #
-      def error?() return false unless @data[:error]; @data[:error] end
+      def error?
+        !!@data[:error]
+      end
 
-      #
       # Sets this state as an error state.
-      #
-      def error!() @data[:error] = true end
+      def error!
+        @data[:error] = true
+      end
 
-      #
       # Returns true if this state is deterministic, false otherwise.
-      #
       def deterministic?
         outs = out_symbols
         (outs.size==@out_edges.size) and not(outs.include?(nil))
@@ -205,117 +203,95 @@ module Stamina
         !accepting? && out_edges.all?{|e| e.target==self}
       end
 
-      #
       # Returns an array containing all incoming edges of the state. Edges are
       # sorted if _sorted_ is set to true. If two incoming edges have same symbol
       # no order is guaranteed between them.
       #
       # Returned array may be modified.
-      #
       def in_edges(sorted=false)
         sorted ? @in_edges.sort : @in_edges.dup
       end
 
-      #
       # Returns an array containing all outgoing edges of the state. Edges are
       # sorted if _sorted_ is set to true. If two outgoing edges have same symbol
       # no order is guaranteed between them.
       #
       # Returned array may be modified.
-      #
       def out_edges(sorted=false)
         sorted ? @out_edges.sort : @out_edges.dup
       end
 
-      #
       # Returns an array with the different symbols appearing on incoming edges.
       # Returned array does not contain duplicates. Symbols are sorted in the
       # array if _sorted_ is set to true.
       #
       # Returned array may be modified.
-      #
       def in_symbols(sorted=false)
         symbols = @in_edges.collect{|e| e.symbol}.uniq
         return sorted ? (symbols.sort &automaton.symbols_comparator) : symbols
       end
 
-      #
       # Returns an array with the different symbols appearing on outgoing edges.
       # Returned array does not contain duplicates. Symbols are sorted in the
       # array if _sorted_ is set to true.
       #
       # Returned array may be modified.
-      #
       def out_symbols(sorted=false)
         symbols = @out_edges.collect{|e| e.symbol}.uniq
         return sorted ? (symbols.sort &automaton.symbols_comparator) : symbols
       end
 
-      #
       # Returns an array with adjacent states (in or out edge).
       #
       # Returned array may be modified.
-      #
       def adjacent_states()
         (in_adjacent_states+out_adjacent_states).uniq
       end
 
-      #
       # Returns an array with adjacent states along an incoming edge (without
       # duplicates).
       #
       # Returned array may be modified.
-      #
       def in_adjacent_states()
         (@in_edges.collect {|e| e.source}).uniq
       end
 
-      #
       # Returns an array with adjacent states along an outgoing edge (whithout
       # duplicates).
       #
       # Returned array may be modified.
-      #
       def out_adjacent_states()
         (@out_edges.collect {|e| e.target}).uniq
       end
 
-      #
       # Returns reachable states from this one with an input _symbol_. Returned
       # array does not contain duplicates and may be modified. This method if not
       # epsilon symbol aware.
-      #
       def step(symbol)
         @out_edges.select{|e| e.symbol==symbol}.collect{|e| e.target}
       end
 
-      #
       # Returns the state reached from this one with an input _symbol_, or nil if
       # no such state. This method is not epsilon symbol aware. Moreover it is
       # expected to be used on deterministic states only. If the state is not
       # deterministic, the method returns one reachable state if such a state
       # exists; which one is returned must be considered non deterministic.
-      #
       def dfa_step(symbol)
         edge = @out_edges.find{|e| e.symbol==symbol}
         edge ? edge.target : nil
       end
 
-      #
       # Computes the epsilon closure of this state. Epsilon closure is the set of
       # all states reached from this one with a <tt>eps*</tt> input (sequence of
       # zero or more epsilon symbols). The current state is always contained in
       # the epsilon closure. Returns an unsorted array without duplicates; this
       # array may not be modified.
-      #
       def epsilon_closure()
         @epsilon_closure ||= compute_epsilon_closure(Set.new).to_a.freeze
       end
 
-      #
       # Internal implementation of epsilon_closure. _result_ is expected to be
       # a Set instance, is modified and is the returned value.
-      #
       def compute_epsilon_closure(result)
         result << self
         step(nil).each do |t|
@@ -325,7 +301,6 @@ module Stamina
         return result
       end
 
-      #
       # Computes an array representing the set of states that can be reached from
       # this state with a given input _symbol_. Returned array does not contain
       # duplicates and may be modified. No particular ordering of states in the
@@ -335,7 +310,6 @@ module Stamina
       # deterministic automata, meaning that it actually computes the set of
       # reachable states through strings respecting the <tt>eps* symbol eps*</tt>
       # regular expression, where eps is the epsilon symbol.
-      #
       def delta(symbol)
         if automaton.deterministic?
           target = dfa_delta(symbol)
@@ -359,7 +333,6 @@ module Stamina
         end
       end
 
-      #
       # Returns the target state that can be reached from this state with _symbol_
       # input. Returns nil if no such state exists.
       #
@@ -368,18 +341,15 @@ module Stamina
       # non deterministic automata, it returns a state immediately reachable from
       # this state with _symbol_ input, or nil if no such state exists. This
       # method is not epsilon aware.
-      #
       def dfa_delta(symbol)
         return nil if symbol.nil?
         edge = @out_edges.find{|e| e.symbol==symbol}
         edge.nil? ? nil : edge.target
       end
 
-      #
       # Provides comparator of states, based on the index in the automaton state
       # list. This method returns nil unless  _o_ is a State from the same
       # automaton than self.
-      #
       def <=>(o)
         return nil unless State===o
         return nil unless automaton===o.automaton
@@ -594,10 +564,8 @@ module Stamina
     ### public read-only section #################################################
     public
 
-    #
     # Returns a symbols comparator taking epsilon symbols into account. Comparator
     # is provided as Proc instance which is a lambda function.
-    #
     def symbols_comparator
       @symbols_comparator ||= Kernel.lambda do |a,b|
         if a==b then 0
